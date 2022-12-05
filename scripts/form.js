ef2c51eaf4f8a2c77d5cd8fc5fcae8d73270c5fe
ssantiago90 <salkin.santiago@gmail.com>
2022-12-05 19:19:43 -0300

    update form

--- scripts/form.js
+++ scripts/form.js
@@ -1,4 +1,9 @@
 console.log("%cloaded script form.js👌", "background: purple");
 
 let formulario = document.getElementById("loginForm");
+
+formulario.addEventListener("submit", () => {});
+
+document.createElement("p");
+
 console.log(formulario);
