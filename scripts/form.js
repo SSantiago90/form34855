651719d1a9d37257172d7b2e9d8d099c20d30d27
ssantiago90 <salkin.santiago@gmail.com>
2024-11-26 15:09:33 -0300

    removed comments

--- scripts/form.js
+++ scripts/form.js
@@ -1,10 +1,1 @@
 console.log("%cloaded script form.js👌", "background: purple");
-
-//1. Agregar un Event Listener al formulario "#loginForm" del index.html
-//2. Obtener el evento en una función (Event handler) y evitar que la página se "refresque"
-//3. Obtener los valores (values) de los inputs Usuario y Contraseña
-//4. Si el usuario es igual a "coderhouse" y la contraseña es igual a 1234,
-//  darle visibilidad a la <section> oculta del index.html
-
-// 5. Similar a la consigna anterior, obtener el "value" del input del formulario
-// 6. Guardar ese valor en una variable y convertirlo en un array (tip: array.split())
